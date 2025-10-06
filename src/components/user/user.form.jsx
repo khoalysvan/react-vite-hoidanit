a7751cbdd3a8c3b6ac792ed405917097259964db
khoalysvan <khoaditrach2004@gmail.com>
2025-10-07 00:48:54 +0700

    Video70 config axios interceptor

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,6 +1,24 @@
-import { Input, Button } from "antd";
+import { Input, Button, notification } from "antd";
+import { useState } from "react";
+import axios from "axios";
+import { createUserAPI } from "../../services/api.service";
 
 const UserForm = () => {
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
+
+    const handleClickBtn = async () => {
+        const res = await createUserAPI(fullName, email, password, phone);
+        console.log(">>> check res: ", res);
+        if (res.data) {
+            notification.success({
+                message: "create user",
+                description: "Tạo mới user thành công",
+            });
+        }
+    };
     return (
         <div className="user-form" style={{ margin: "20px 0" }}>
             <div
@@ -12,22 +30,36 @@ const UserForm = () => {
             >
                 <div>
                     <span>Full Name</span>
-                    <Input />
+                    <Input
+                        value={fullName}
+                        onChange={(event) => setFullName(event.target.value)}
+                    />
                 </div>
                 <div>
                     <span>Email</span>
-                    <Input />
+                    <Input
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
                 </div>
                 <div>
                     <span>Password</span>
-                    <Input.Password />
+                    <Input.Password
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
                 </div>
                 <div>
                     <span>Phone</span>
-                    <Input />
+                    <Input
+                        value={phone}
+                        onChange={(event) => setPhone(event.target.value)}
+                    />
                 </div>
                 <div>
-                    <Button type="primary">Create User</Button>
+                    <Button type="primary" onClick={handleClickBtn}>
+                        Create User
+                    </Button>
                 </div>
             </div>
         </div>
