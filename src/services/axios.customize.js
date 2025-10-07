e56d6f87fb9dffee957384888d423edc59c2dc05
khoalysvan <khoaditrach2004@gmail.com>
2025-10-07 11:07:07 +0700

    Video73 useEffect hook

--- src/services/axios.customize.js
+++ src/services/axios.customize.js
@@ -22,7 +22,6 @@ instance.interceptors.response.use(
     function (response) {
         // Any status code that lie within the range of 2xx cause this function to trigger
         // Do something with response data
-        debugger;
         console.log(">>> check response: ", response);
         if (response.data && response.data.data) return response.data;
         return response;
