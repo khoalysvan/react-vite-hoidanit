6f59fe3a1a1991b3c61854e753412cc5114f624b
khoalysvan <khoaditrach2004@gmail.com>
2025-10-10 16:46:46 +0700

    Video80 delete user

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,7 +1,9 @@
-import { Table } from "antd";
+import { Table, Popconfirm, notification } from "antd";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import UpdateUserModal from "./update.user.modal";
+import ViewUserDetail from "./view.user.detail";
 import { useState } from "react";
+import { deleteUserAPI } from "../../services/api.service";
 
 const UserTable = (props) => {
     const { dataUsers, loadUser } = props;
@@ -9,12 +11,41 @@ const UserTable = (props) => {
     const [isModalUpdateOpen, setIsModalUpdateOpen] = useState(false);
 
     const [dataUpdate, setDataUpdate] = useState(null);
+
+    const [isDetailOpen, setIsDetailOpen] = useState(false);
+    const [dataDetail, setDataDetail] = useState(null);
+
+    const handleDeleteUser = async (id) => {
+        const res = await deleteUserAPI(id);
+        if (res.data) {
+            notification.success({
+                message: "Delete User",
+                description: "Xóa user thành công",
+            });
+            await loadUser();
+        } else {
+            notification.error({
+                message: "Error delete user",
+                description: JSON.stringify(res.message),
+            });
+        }
+    };
     const columns = [
         {
             title: "Id",
             dataIndex: "_id",
             render: (_, record) => {
-                return <a href="#">{record._id}</a>;
+                return (
+                    <a
+                        href="#"
+                        onClick={() => {
+                            setDataDetail(record);
+                            setIsDetailOpen(true);
+                        }}
+                    >
+                        {record._id}
+                    </a>
+                );
             },
         },
         {
@@ -37,9 +68,19 @@ const UserTable = (props) => {
                             setIsModalUpdateOpen(true);
                         }}
                     />
-                    <DeleteOutlined
-                        style={{ cursor: "pointer", color: "red" }}
-                    />
+                    <Popconfirm
+                        title="Delete the task"
+                        description="Are you sure to delete this task?"
+                        placement="left"
+                        onConfirm={() => handleDeleteUser(record._id)}
+                        // onCancel={cancel}
+                        okText="Yes"
+                        cancelText="No"
+                    >
+                        <DeleteOutlined
+                            style={{ cursor: "pointer", color: "red" }}
+                        />
+                    </Popconfirm>
                 </div>
             ),
         },
@@ -54,6 +95,12 @@ const UserTable = (props) => {
                 dataUpdate={dataUpdate}
                 setDataUpdate={setDataUpdate}
                 loadUser={loadUser}
+            />
+            <ViewUserDetail
+                dataDetail={dataDetail}
+                setDataDetail={setDataDetail}
+                isDetailOpen={isDetailOpen}
+                setIsDetailOpen={setIsDetailOpen}
             />
         </>
     );
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -24,4 +24,9 @@ const fetchAllUserAPI = () => {
     const URL_BACKEND = "/api/v1/user";
     return axios.get(URL_BACKEND);
 };
-export { createUserAPI, updateUserAPI, fetchAllUserAPI };
+
+const deleteUserAPI = (id) => {
+    const URL_BACKEND = `/api/v1/user/${id}`;
+    return axios.delete(URL_BACKEND);
+};
+export { createUserAPI, updateUserAPI, fetchAllUserAPI, deleteUserAPI };
